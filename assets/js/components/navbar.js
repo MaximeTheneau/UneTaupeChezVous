072ee88fa657535470a632c28190d7724a4e4102
MaximeThe <theneau.maxime@gmail.com>
2022-06-01 19:44:43 +0200

    img responsives

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -4,16 +4,6 @@ const navbar = {
     },
     stickyHandlerNavbar: function()
     {
-        const navbarElt = document.querySelector(".navbar");
-        const headerImg = document.querySelector(".headerImg");
-        const sticky = navbarElt.offsetTop;
-        if (window.scrollY >= sticky) {
-            navbarElt.classList.add("is-fixed-top");
-            headerImg.classList.add("none");
-          } else if (window.scrollX >= sticky) {
-            navbarElt.classList.remove('is-fixed-top');
-            headerImg.classList.remove("none");
 
-          }
     }
 }
