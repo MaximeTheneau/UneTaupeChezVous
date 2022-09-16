b92cf3a7da9b48941fee7be4e3c2c114f850a8f3
Theneau Maxime <theneau.maxime@gmail.com>
2022-09-16 14:13:56 +0200

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,27 @@
 // == Import
 import { useDispatch, useSelector } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./styles.css";
 import {
   imgStickyFalse,
   toogleNavbar,
   toogleNavbarSubtitle,
 } from "../../action/header";
-import Spinner from "../Spinner";
-import "./styles.css";
 
-
+// == Img
+import ImgSmallJardin from "../../assets/webp/illustration-jardin-taupe-small.webp";
+import ImgMiddleJardin from "../../assets/webp/illustration-jardin-taupe-middle.webp";
+import ImgLargeJardin from "../../assets/webp/illustration-jardin-taupe-large.webp";
+import ImgJardin from "../../assets/svg/illustration-jardin-taupe.svg";
+import ImgLogo from "../../assets/svg/logo-une-taupe-chez-vous.svg";
+import ImgLogoWebp from "../../assets/webp/logo-une-taupe-chez-vous.webp";
 // == Composant
 function Header() {
   const dispatch = useDispatch();
   const toggleNav = useSelector((state) => state.header.toogleNavbar);
   const imgSticky = useSelector((state) => state.header.imgSticky);
-  const spinnerLoad = useSelector((state) => state.header.spinner);
   const toggleNavSubtitle = useSelector(
-    (state) => state.header.toogleNavbarSubtitle
+    (state) => state.header.toogleNavbarSubtitle,
   );
   const handleSubtitle = () => {
     dispatch(toogleNavbarSubtitle());
@@ -25,160 +29,161 @@ function Header() {
   };
   return (
     <>
-      {spinnerLoad ? (<Spinner /> ) : (
-        <>
-        {imgSticky ? (
-          <div className="header-sticky">
-            <picture  >
-              <source
-                srcSet={require("../../assets/webp/illustration-jardin-taupe-small.webp")}
-                media="(max-width: 550px)"
-                type="image/webp"
-              />
-              <source
-                srcSet={require("../../assets/webp/illustration-jardin-taupe-middle.webp")}
-                media="(max-width: 1024px)"
-                type="image/webp"
-              />
-              <source
-                srcSet={require("../../assets/webp/illustration-jardin-taupe-large.webp")}
-                media="(max-width: 2000px)"
-                type="image/webp"
-              />
-              <img
-                className="header-sticky_img"
-                src={require("../../assets/svg/illustration-jardin-taupe.svg?url")}
-                alt="Illustration d'une taupe dans un jardin avec une tondeuse"
-                title="illustration Une taupe chez vous"
-                width="100%"
-                height="100%"
-              />
-            </picture>
-            <div
-              className="header-sticky-content"
-              onClick={() => dispatch(imgStickyFalse())}
-            >
-              <a href="#bienvenue" name="icon scroll" >
-                <i className="icon-angle-90" />
-              </a>
+      {imgSticky ? (
+        <div className="header-sticky">
+          <picture>
+            <source
+              srcSet={ImgSmallJardin}
+              media="(max-width: 550px)"
+              type="image/webp"
+            />
+            <source
+              srcSet={ImgMiddleJardin}
+              media="(max-width: 1024px)"
+              type="image/webp"
+            />
+            <source
+              srcSet={ImgLargeJardin}
+              media="(max-width: 2000px)"
+              type="image/webp"
+            />
+            <img
+              className="header-sticky_img"
+              src={ImgJardin}
+              alt="Illustration d'une taupe dans un jardin avec une tondeuse"
+              title="illustration Une taupe chez vous"
+              width="100%"
+              height="100%"
+            />
+          </picture>
+          <div
+            className="header-sticky-content"
+            onClick={() => dispatch(imgStickyFalse())}
+          >
+            <a href="#bienvenue" name="icon scroll" >
+              <i className="icon-angle-90" />
+            </a>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <header className="header" id="bienvenue">
+        {toggleNav ? (
+          <div className="header-navbar-toggle">
+            <div className="header-logo">
+              <NavLink
+                to="/"
+                onClick={() => {
+                  dispatch(toogleNavbar());
+                  dispatch(imgStickyFalse());
+                }}
+              >
+                <picture>
+                  <source
+                    srcSet={ImgLogoWebp}
+                    type="image/webp"
+                  />
+                  <img
+                    src={ImgLogo}
+                    alt="Logo Une Taupe Chez Vous"
+                    title="Logo Une taupe chez vous"
+                    className="logo"
+                    width="100%"
+                    height="100%"
+                  />
+                </picture>
+              </NavLink>
+            </div>
+            <div className="header-button_close">
+              <button
+                type="button"
+                id="button_nav"
+                title="Fermer le menu"
+                onClick={() => {
+                  dispatch(toogleNavbar());
+                  dispatch(imgStickyFalse());
+                }}
+              >
+                <i className="icon-navbar" />
+              </button>
             </div>
           </div>
         ) : (
-          ""
-        )}
-        <header className="header" id="bienvenue">
-          {toggleNav ? (
+          <nav className="navbar">
             <div className="header-navbar-toggle">
               <div className="header-logo">
-                <NavLink
-                  to="/"
-                  onClick={() => {
-                    dispatch(toogleNavbar());
-                    dispatch(imgStickyFalse());
-                  }}
-                >
+                <NavLink to="/">
                   <picture>
                     <source
-                      srcSet={require("../../assets/webp/logo-une-taupe-chez-vous.webp")}
+                      srcSet={ImgLogoWebp}
                       type="image/webp"
                     />
                     <img
-                      src={require("../../assets/svg/logo-une-taupe-chez-vous.svg?url")}
+                      src={ImgLogo}
                       alt="Logo Une Taupe Chez Vous"
                       title="Logo Une taupe chez vous"
-                      className="logo"
-                      width="100%"
-                      height="100%"
                     />
                   </picture>
                 </NavLink>
               </div>
               <div className="header-button_close">
                 <button
-                  id="button_nav"
-                  title="Fermer le menu"
-                  onClick={() => {
-                    dispatch(toogleNavbar());
-                    dispatch(imgStickyFalse());
-                  }}
+                  type="button"
+                  onClick={() => dispatch(toogleNavbar())}
                 >
-                  <i className="icon-navbar" />
+                  <i className="icon-x" />
                 </button>
               </div>
             </div>
-          ) : (
-            <nav className="navbar">
-              <div className="header-navbar-toggle">
-                <div className="header-logo">
-                  <NavLink to="/">
-                    <picture>
-                      <source
-                        srcSet={require("../../assets/webp/logo-une-taupe-chez-vous.webp")}
-                        type="image/webp"
-                      />
-                      <img
-                        src={require("../../assets/svg/logo-une-taupe-chez-vous.svg?url")}
-                        alt="Logo Une Taupe Chez Vous"
-                        title="Logo Une taupe chez vous"
-                      />
-                    </picture>
-                  </NavLink>
-                </div>
-                <div className="header-button_close">
-                  <button onClick={() => dispatch(toogleNavbar())}>
-                    <i className="icon-x" />
-                  </button>
-                </div>
-              </div>
-              <ul className="header-navbar">
-                <NavLink to="/" onClick={() => dispatch(toogleNavbar())}>
-                  <li className="header-navbar-item">Accueil</li>
-                </NavLink>
-                <li>
-                  <div
-                    className="header-navbar-item_subtitle"
-                    onClick={() => dispatch(toogleNavbarSubtitle())}
-                  >
-                    Services
-                    {toggleNavSubtitle ? (
-                      <i className="icon-angle" />
-                    ) : (
-                      <i className="icon-angle-90" />
-                    )}
-                  </div>
+            <ul className="header-navbar">
+              <NavLink to="/" onClick={() => dispatch(toogleNavbar())}>
+                <li className="header-navbar-item">Accueil</li>
+              </NavLink>
+              <li>
+                <div
+                  className="header-navbar-item_subtitle"
+                  onClick={() => dispatch(toogleNavbarSubtitle())}
+                >
+                  Services
                   {toggleNavSubtitle ? (
-                    ""
+                    <i className="icon-angle" />
                   ) : (
-                    <ul className="header-navbar-subitem">
-                      <NavLink
-                        to="/services/nuisibles/taupe"
-                        onClick={handleSubtitle}
-                      >
-                        <li className="header-navbar-subtitle-item">Taupe</li>
-                      </NavLink>
-                      <NavLink
-                        to="/services/nuisibles/fouine"
-                        onClick={handleSubtitle}
-                      >
-                        <li className="header-navbar-subtitle-item">Fouine</li>
-                      </NavLink>
-                      <NavLink
-                        to="/services/nuisibles/Ragondin"
-                        onClick={handleSubtitle}
-                      >
-                        <li className="header-navbar-subtitle-item">Ragondin</li>
-                      </NavLink>
-                    </ul>
+                    <i className="icon-angle-90" />
                   )}
-                </li>
-                <NavLink to="/contact" onClick={() => dispatch(toogleNavbar())}>
-                  <li className="header-navbar-item">Contact</li>
-                </NavLink>
-              </ul>
-            </nav>
-          )}
-        </header>
-      </>)}
+                </div>
+                {toggleNavSubtitle ? (
+                  ""
+                ) : (
+                  <ul className="header-navbar-subitem">
+                    <NavLink
+                      to="/services/nuisibles/taupe"
+                      onClick={handleSubtitle}
+                    >
+                      <li className="header-navbar-subtitle-item">Taupe</li>
+                    </NavLink>
+                    <NavLink
+                      to="/services/nuisibles/fouine"
+                      onClick={handleSubtitle}
+                    >
+                      <li className="header-navbar-subtitle-item">Fouine</li>
+                    </NavLink>
+                    <NavLink
+                      to="/services/nuisibles/Ragondin"
+                      onClick={handleSubtitle}
+                    >
+                      <li className="header-navbar-subtitle-item">Ragondin</li>
+                    </NavLink>
+                  </ul>
+                )}
+              </li>
+              <NavLink to="/contact" onClick={() => dispatch(toogleNavbar())}>
+                <li className="header-navbar-item">Contact</li>
+              </NavLink>
+            </ul>
+          </nav>
+        )}
+      </header>
     </>
   );
 }
